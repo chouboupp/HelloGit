4a921d91bdb49eefd03a264d0f64d775e2fffad5
chouboupp <audrey.roscop@gmail.com>
2021-03-16 13:39:55 +0100

    Initial Commit

---
+++ HelloGit/HelloGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGitApp.swift
+//  HelloGit
+//
+//  Created by Audrey Roscop on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
